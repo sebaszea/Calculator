72cfeb7478ac2f8efe78e7ff5cfa826362dd6e40
SebasZea <szea2005@gmail.com>
2021-11-16 14:52:36 -0500

    feat: Basic template creation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
- <h1>CALCULADORA</h1>
-<icon color='#c42f21' size='200'/>
+  <div class="bg-gray-800 h-screen grid grid-cols-2"> 
+        <div class="bg-gray-800">
+         <div class="flex justify-center">
+            <h1 class="text-center mt-10 flex items-center"> CALCULADORA <icon color='#33593d' size='25'></icon> </h1>
+         </div>
+         </div>
+
+        
+        <div class="bg-gray-800">
+          <div class="flex justify-center">
+            <h1 class="text-center mt-10 flex items-center"> HISTORIAL <icon2 color='#33593d' size='25'></icon2> </h1>
+          </div>
+          </div>
+        
+  </div>
 
 </template>
 
 <script>
 import Icon from './components/assets/Icon.vue'
+import Icon2 from './components/assets/Icon2.vue'
 export default {
-  components: {Icon}
+  components: {Icon, Icon2} 
 }
 
 </script>
