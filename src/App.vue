3fbdcb11aee8efba534ab861d5321615e7d8ca20
SebasZea <szea2005@gmail.com>
2021-11-17 20:51:11 -0500

    refactor: Change of colors, new icon and new font style

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="bg-gray-800 h-screen grid grid-cols-2"> 
-        <div class="bg-gray-800">
+  <div class="blancof h-screen grid grid-cols-2"> 
+      <div>
          <div class="flex justify-center">
-            <h1 class="text-center mt-10 flex items-center"> CALCULADORA <icon color='#33593d' size='25'></icon> </h1>
+            <h1 class="text-center mt-10 flex items-center font1 text-3xl The quick brown fox"> 
+              <span class="mr-8 azuloscurof">CALCULADORA</span>
+              <icon class="mb-2" color='#33593d' size='35'></icon> 
+            </h1>
          </div>
-         </div>
+     </div>
 
         
-        <div class="bg-gray-800">
-          <div class="flex justify-center">
-            <h1 class="text-center mt-10 flex items-center"> HISTORIAL <icon2 color='#33593d' size='25'></icon2> </h1>
-          </div>
-          </div>
+     <div class="flex flex-col"> 
+         <div class="flex justify-center">
+             <h1 class="text-center mt-10 flex items-center font1 text-3xl The quick brown fox">
+               <span class="mr-8 azuloscurof">HISTORIAL</span>
+                <div class="mb-2"> 
+                 <icon2 color='#33593d' size='35'></icon2>
+                </div>
+             </h1>
+         </div>
+           <div class="flex justify-end items-end w-full flex-grow">
+            <a href="">
+              <icon3 class="mb-8 mr-10" color='#e63946' size='35'></icon3>
+            </a>
+           </div>
+     </div>
         
   </div>
 
@@ -20,8 +33,9 @@
 <script>
 import Icon from './components/assets/Icon.vue'
 import Icon2 from './components/assets/Icon2.vue'
+import Icon3 from './components/Icon3.vue'
 export default {
-  components: {Icon, Icon2} 
+  components: {Icon, Icon2, Icon3} 
 }
 
 </script>
